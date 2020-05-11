d8ba6cdc3e01d56acee2cc8646e03e824cb3b45a
David <david.erving@hotmail.com>
2020-05-11 10:35:49 +1000

    added layla

--- create_current_temp.py
+++ create_current_temp.py
@@ -12,20 +12,24 @@ server_address = "192.168.0.10"
 topic_temp_lounge = "home/inside/sensor/lounge"
 topic_temp_master = "home/inside/sensor/master"
 topic_temp_joel = "home/inside/sensor/joel"
+topic_temp_layla = "home/inside/sensor/layla"
 
 topic_status_lounge = "status/sensor/lounge"
 topic_status_master = "status/sensor/master"
 topic_status_joel = "status/sensor/joel"
+topic_status_layla = "status/sensor/layla"
 
 output_topic = "home/inside/sensor/CurrentTemp"
 
 lounge_temp = 21
 master_temp = 21
 joel_temp = 21
+layla_temp = 21
 
 lounge_status = "online"
 master_status = "online"
 joel_status = "online"
+layla_status = "online"
 
 temp_list = []
 
@@ -38,7 +42,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe([(topic_status_lounge,0),(topic_temp_lounge,0),(topic_status_master,0),(topic_temp_master,0),(topic_status_joel,0),(topic_temp_joel,0)])
+    client.subscribe([(topic_status_lounge,0),(topic_temp_lounge,0),(topic_status_master,0),(topic_temp_master,0),(topic_status_joel,0),(topic_temp_joel,0),(topic_status_layla,0),(topic_temp_layla,0)])
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -48,6 +52,8 @@ def on_message(client, userdata, msg):
     global master_status
     global joel_temp
     global joel_status    
+    global layla_temp
+    global layla_status  
     global temp_list
     global period_start
 
@@ -75,6 +81,12 @@ def on_message(client, userdata, msg):
     elif topic == topic_temp_joel:
         joel_temp = jsonData["temperature"]
 
+    elif topic == topic_status_layla:
+        layla_status = jsonData["status"]
+
+    elif topic == topic_temp_layla:
+        layla_temp = jsonData["temperature"]
+
     #print(lounge_status, lounge_temp, master_status, master_temp, joel_status, joel_temp)
 
     if lounge_status == "online":
@@ -82,6 +94,9 @@ def on_message(client, userdata, msg):
     
     elif master_status == "online":
         temp = lounge_temp
+
+    elif layla_status == "online":
+        temp = layla_temp
 
     else:
         temp = joel_temp
